1d37f9911b46e0bffa3ea229d7a548c5dae884ef
MalapatiPreethisree <119839549+MalapatiPreethisree@users.noreply.github.com>
2024-01-11 11:46:01 +0530

    import java.util.Scanner; public class Weight{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int b1=sc.nextInt();         int b2=sc.nextInt();         System.out.printf("%d",(3*x)-(b1+b2));     } }

---
+++ Average_weight.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Weight{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int b1=sc.nextInt();
+        int b2=sc.nextInt();
+        System.out.printf("%d",(3*x)-(b1+b2));
+    }
+}
