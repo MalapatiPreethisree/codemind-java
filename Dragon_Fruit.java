d09e96b69d1167bd494f88bc11e01c70ae1149ad
MalapatiPreethisree <119839549+MalapatiPreethisree@users.noreply.github.com>
2024-01-19 20:47:04 +0530

    import java.util.Scanner; public class Dragonfruit{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         if(x%2==0 && x>2)         System.out.print("YES");         else         System.out.print("NO");     } }

---
+++ Dragon_Fruit.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Dragonfruit{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x%2==0 && x>2)
+        System.out.print("YES");
+        else
+        System.out.print("NO");
+    }
+}
